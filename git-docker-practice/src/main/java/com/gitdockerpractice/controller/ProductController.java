f8784d5055857e9be17d80c1b8563c71d623b796
shahrukh khan <hiiisrk01@gmail.com>
2024-05-02 11:12:59 +0530

    isme main yml and docker configuration dale hai,aur ab project sudhara hai

--- git-docker-practice/src/main/java/com/gitdockerpractice/controller/ProductController.java
+++ git-docker-practice/src/main/java/com/gitdockerpractice/controller/ProductController.java
@@ -2,9 +2,15 @@ package com.gitdockerpractice.controller;
 
 import com.gitdockerpractice.entity.Product;
 import com.gitdockerpractice.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.auditing.CurrentDateTimeProvider;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
 
 @Component
 @ResponseBody
@@ -17,6 +23,8 @@ public class ProductController {
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+    Logger logger =LoggerFactory.getLogger(ProductController.class);
+
 
 //http://localhost:8080/api/v1/products/saveProduct
     
@@ -24,9 +32,13 @@ public class ProductController {
     @ResponseStatus(HttpStatus.OK)
     public String saveProduct(@RequestBody Product product){
 
+           logger.info("Before Saving"+" " +new Date());
+
         Product save = productRepository.save(product);
 
         System.out.println(product);
+
+        logger.info("After Saving"+ new Date());
 
         return "product is saved";
 
